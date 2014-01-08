bc08b56f6041a17ee2f5a5812d61552a3ab9230d
xperiandri <xperiandri@live.ru>
2014-01-08 19:39:47 +0200

    Added license and readme. Fixed some XML comments.

--- StringBuilderExtensions/StringBuilderExtensions.cs
+++ StringBuilderExtensions/StringBuilderExtensions.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿#region License
+// The MIT License (MIT)
+
+// Copyright (c) 2014 Andrii Chebukin as XperiAndri
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#endregion License
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Globalization;
@@ -281,7 +305,7 @@ namespace System.Text
         }
 
         /// <summary>
-        /// Removes all trailing occurrences of a set of characters specified in an array
+        /// Removes all leading and trailing occurrences of a set of characters specified in an array
         /// from the current <see cref="System.Text.StringBuilder"/> object.
         /// </summary>
         /// <param name="sb">A <see cref="System.Text.StringBuilder"/> to </param>
@@ -705,7 +729,8 @@ namespace System.Text
 
 
         /// <summary>
-        /// Reports the zero-based index of the last occurrence of the specified string in the current <see cref="System.Text.StringBuilder"/> object.
+        /// Reports the zero-based index of the last occurrence of the specified string 
+        /// in the current <see cref="System.Text.StringBuilder"/> object.
         /// </summary>
         /// <param name="sb">A <see cref="System.Text.StringBuilder"/> to </param>
         /// <param name="value">The string to seek.</param>
@@ -928,7 +953,7 @@ namespace System.Text
         }
 
         /// <summary>
-        /// Determine whether a string is begin with a given text.
+        /// Determines whether this instance of <see cref="System.Text.StringBuilder"/> starts with the specified string.
         /// </summary>
         /// <param name="sb">A <see cref="System.Text.StringBuilder"/> to compare.</param>
         /// <param name="value">The string to compare.</param>
@@ -970,7 +995,7 @@ namespace System.Text
         }
 
         /// <summary>
-        /// Determines whether the end of this <see cref="System.Text.StringBuilder"/> instance matches the specified string.
+        /// Determines whether this instance of <see cref="System.Text.StringBuilder"/> ends with the specified string.
         /// </summary>
         /// <param name="sb">A <see cref="System.Text.StringBuilder"/> to compare.</param>
         /// <param name="value">The string to compare to the substring at the end of this instance.</param>
---
+++ readme.md
@@ -0,0 +1,20 @@
+# String Builder Extensions #
+String Builder Extensions is a set of extension methods for **[System.Text.StringBuilder](http://msdn.microsoft.com/en-us/library/system.text.stringbuilder(v=vs.110).aspx)** that provide the same functionality as corresponding methods of **[System.String](http://msdn.microsoft.com/en-us/library/system.string.aspx)**
+
+Available methods are:
+
+- **Remove** - Removes all occurences of specified characters from StringBuilder.
+- **Remove** - Removes the range of characters from the specified index to the end of StringBuilder.
+- **StartsWith** - Determines whether this instance of StringBuilder starts with the specified string.
+- **EndWith** - Determines whether this instance of StringBuilder ends with the specified string.
+- **IndexOf** (6 overrides) - Reports the zero-based index position of the first occurrence of the specified Unicode character within this instance.
+- **LastIndexOf** (6 overrides) - Reports the zero-based index of the last occurrence of the specified string in the current StringBuilder object. 
+- **IndexOfAny** (3 overrides) - Reports the zero-based index of the first occurrence in this instance of any character in a specified array of Unicode characters. 
+- **LastIndexOfAny** (3 overrides) - Reports the zero-based index of the last occurrence in this instance of any character in a specified array of Unicode characters.
+- **Trim** (2 overrides) - Removes all leading and trailing occurrences of a set of characters specified in an array from the current StringBuilder object.
+- **TrimStart** - Removes all leading occurrences of a set of characters specified in an array from the current StringBuilder object.
+- **TrimEnd** - Removes all trailing occurrences of a set of characters specified in an array from the current StringBuilder object.
+- **ToLower** (2 overrides) - Returns a StringBuilder converted to lowercase, using the casing rules of the specified culture.
+- **ToLowerInvariant** - Returns a StringBuilder converted to lowercase, using the casing rules of the invarian culture.
+- **ToUpper** (2 overrides) - Returns a StringBuilder converted to uppercase, using the casing rules of the specified culture.
+- **ToUpperInvariant** - Returns a StringBuilder converted to uppercase, using the casing rules of the invariant culture.
